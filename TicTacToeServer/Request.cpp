444b0af083de924bcaea70c091c07333948da332
Shanoi <olivboulet@laposte.net>
2016-04-05 10:10:57 +0200

    Comm pour les tests

--- TicTacToeServer/Request.cpp
+++ TicTacToeServer/Request.cpp
@@ -16,6 +16,11 @@ Request::~Request(void)
 }
 
 void Request::setIdentity(Server* server, setIdentityRequestQuery query) {
+
+	//Tester si le pseudo est mal formaté
+
+	//Tester si le pseudo est déja pris
+
 	std::cout << "*setIdentity" << std::endl;
 }
 
@@ -25,7 +30,7 @@ void Request::getIdentity(Server* server, getIdentityRequestQuery query) {
 
 	std::vector<Player*> clientsTarget;
 	clientsTarget.push_back(query.sender);
-	
+
 	Errors errors;
 	errors.addError(478, 65, "dfndkjfgnjdkf");;
 
@@ -39,33 +44,76 @@ void Request::getIdentity(Server* server, getIdentityRequestQuery query) {
 }
 
 void Request::createMatch(Server* server, createMatchRequestQuery query) {
+	
+	//Tester la taille de la grille
+
+	//Tester la taille de la winsize, supérieur ou égale à 3, et que cette valeur soit possible à atteindre
+	//Ex : dans une grille 3 x 3, on ne peut pas aligner 4 pions
+
+	//Tester si les players sont valides, pseudos bien formatés...
+
 	std::cout << "*createMatch" << std::endl;
 }
 
 void Request::getMatchList(Server* server, getMatchListRequestQuery query) {
+
+	//Aucun test
+
 	std::cout << "*getMatchList" << std::endl;
 }
 
 void Request::getMatchInformation(Server* server, getMatchInformationRequestQuery query) {
-		std::cout << "*getMatchInformation" << std::endl;
+
+	//Tester si le match existe
+
+	std::cout << "*getMatchInformation" << std::endl;
 }
 
 void Request::joinMatch(Server* server, joinMatchRequestQuery query) {
-		std::cout << "*joinMatch" << std::endl;
+
+	//Tester si le match existe
+
+	//Tester si le mode de suivit existe "PLAYER" ou "SPECTATOR"
+
+	//Tester si le joueur est invité dans la partie
+
+	//Tester si il y a assez dez place dans la partie (dans le cas où l'utilisateur pouvait rejoindre la partie?)
+
+	std::cout << "*joinMatch" << std::endl;
 }
 
 void Request::playMatch(Server* server, playMatchRequestQuery query) {
-		std::cout << "*playMatch" << std::endl;
+
+	//Tester si la partie est finie
+
+	//Tester si c'est le tour du joueur
+
+	//Tester si les coordonnées sont valides, comprises dans le plateau ...
+
+	//Tester si la case est occupée
+
+	std::cout << "*playMatch" << std::endl;
 }
 
 void Request::resetMatch(Server* server, resetMatchRequestQuery query) {
+
+	//Tester si le match existe
+
 	std::cout << "*resetMatch" << std::endl;
 }
 
 void Request::quitMatch(Server* server, quitMatchRequestQuery query) {
+
+	//Tester si le match existe
+
 	std::cout << "*quitMatch" << std::endl;
 }
 
 void Request::getPlayerInformation(Server* server, getPlayerInformationRequestQuery query) {
+	
+	//Tester si le pseudo est au bon format
+
+	//Tester si le joueur existe
+
 	std::cout << "*getPlayerInformation" << std::endl;
 }
